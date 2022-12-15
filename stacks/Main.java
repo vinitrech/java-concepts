b3dc88d5bd68d2cf6d5c5fe671e7da2618a44264
Vinícius Tonello Rech <vinitrech@pop-os.localdomain>
2022-12-15 16:16:50 -0300

    testing

--- stacks/Main.java
+++ stacks/Main.java
@@ -7,13 +7,12 @@ public class Main {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
         List<Integer> stack2 = new Stack<>();
-        stack2.add(1); // add actually calls the pushs() method from Stack
+        stack2.add(1); // add actually calls the push() method from Stack
 
         stack.push(1);
         stack.push(2);
         stack.push(3);
         stack.push(4);
-        stack.push(5);
 
         System.out.println(stack);
         System.out.println(stack.peek());
